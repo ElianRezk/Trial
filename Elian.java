3b1d5fb355e3c6431ae7fbc911bacb1fd9e4ab1a
elian.rezk@lau.edu <elian.rezk@lau.edu>
2023-03-14 20:12:53 +0200

    the name is elian rezk

---
+++ Elian.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Elian {
+	public static void main (String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		Random rnd = new Random();
+
+	String number = "Elian Rezk";
+
+
+	char x = number.charAt(number.length());
+System.out.println(x);
+
+
+
+		
+
+
+
+		
+
+
+
+
+
+
+	
+		}
+	}
---
+++ Trial/idk.java
@@ -0,0 +1,17 @@
+public class idk{
+	public static void main (String[] args){
+
+
+
+		System.out.println("Elian Rezk");
+
+
+
+
+
+
+
+
+
+	}
+}
